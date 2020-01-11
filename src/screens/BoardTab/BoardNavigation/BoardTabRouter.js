42b4d052b48730beee337c5d061ba7b99522afd1
yjw5344 <yjw5344@hanmail.net>
2020-01-12 01:53:39 +0900

    2020.01.12 feature/Board 게시판상단탭바 완료

---
+++ src/screens/BoardTab/BoardNavigation/BoardTabRouter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Icon } from 'native-base'; 
+import { Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs'
+import {createAppContainer } from 'react-navigation'
+import BoardTabTotal from '../Tab/BoardTotal'
+
+const BoardTabNavigator = createMaterialTopTabNavigator({
+  전체: {
+    screen: BoardTabTotal,
+    initialRouteName : '전체',
+  },
+  정보: {
+    screen:BoardTabTotal    
+  },
+  홍보: {
+    screen:BoardTabTotal    
+  },
+  단체: {
+    screen:BoardTabTotal    
+  },
+},
+{
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarOptions: {
+    pressColor: 'black',
+    style: {
+      backgroundColor: 'white',
+    },
+    indicatorStyle: {
+      backgroundColor: 'black',
+    },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    showLabel: true,
+    showIcon: true,
+  },
+}
+);
+
+export default createAppContainer(BoardTabNavigator);
