c7d50be38d00d1aa4f0c6a2922beb1d62ee7616d
Sikso <sikso1897@gamil.com>
2020-01-12 11:37:08 +0900

    Add Content Card Dummy template

--- src/screens/HomeTab/HomeTabPresenter.js
+++ src/screens/HomeTab/HomeTabPresenter.js
@@ -55,6 +55,52 @@ class HomeTabTitle extends React.Component {
   }
 }
 
+let images = [
+  "https://cdn.pixabay.com/photo/2018/11/29/21/19/hamburg-3846525__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/11/11/16/51/ibis-3809147__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/11/23/14/19/forest-3833973__480.jpg",
+  "https://cdn.pixabay.com/photo/2019/01/05/17/05/man-3915438__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/12/04/22/38/road-3856796__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/11/04/20/21/harley-davidson-3794909__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/12/25/21/45/crystal-ball-photography-3894871__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/12/29/23/49/rays-3902368__480.jpg",
+  "https://cdn.pixabay.com/photo/2017/05/05/16/57/buzzard-2287699__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/08/06/16/30/mushroom-3587888__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/12/15/02/53/flower-3876195__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/12/16/18/12/open-fire-3879031__480.jpg",
+  "https://cdn.pixabay.com/photo/2018/11/24/02/05/lichterkette-3834926__480.jpg",
+"https://cdn.pixabay.com/photo/2018/11/29/19/29/autumn-3846345__480.jpg"
+]
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+const { width, height } = Dimensions.get('window');
+
+class HomeTabContentOne extends React.Component {
+  renderSectionOne = () => {    
+    return images.map((image, index) => {
+      return (
+        <View key={index} 
+              style={{ width: width/3, height: width/3 }} >
+          <Image source={{ url: image }} style={{ flex:1 }}/>
+        </View>
+      )
+    })
+  }
+
+  render(){
+    return(
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{flexDirection:'row'}}>
+          { this.renderSectionOne() }
+      </ScrollView>
+    )
+  }
+}
 export default class HomeTabPresenter extends React.Component{
   render(){
     return(
@@ -63,6 +109,8 @@ export default class HomeTabPresenter extends React.Component{
           
           <HomeTabTitle/>
           
+          <HomeTabContentOne/>
+
         </Content>
       </Container>
     );
