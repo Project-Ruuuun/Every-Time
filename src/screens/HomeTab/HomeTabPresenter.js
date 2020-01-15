302eac72f1fc282c0ff1c881b7c787b67c85f926
Sikso <sikso1897@gamil.com>
2020-01-15 15:06:47 +0900

    solve unique key warning

--- src/screens/HomeTab/HomeTabPresenter.js
+++ src/screens/HomeTab/HomeTabPresenter.js
@@ -106,7 +106,7 @@ class HomeTabContentTwo extends React.Component {
     let buttons=[]
     for (let i=0; i<7; i++){
         buttons.push(
-        <Button transparent style={{flexDirection:'column', width:width/6, height:height/10}}>
+        <Button key={i} transparent style={{flexDirection:'column', width:width/6, height:height/10}}>
           <Icon type="FontAwesome" name="home" style={{fontSize:40}}/>
           <Text style={{fontSize:15, color:'grey'}}>학교</Text>
           <Text style={{fontSize:15, color:'grey'}}>홈</Text>          
@@ -123,11 +123,35 @@ class HomeTabContentTwo extends React.Component {
     )
   }
 }
-
+const sempleOne = [{
+    category: "서울캠 자유게시판",
+    title: "오랜만에 실로암 영상 보니까 웃기네 웃기네"
+  },
+  {
+    category: "비말게시판",
+    title: "인증 후 이용가능"
+  },
+  {
+    category: "졸업생게시판",
+    title: "이번주 업적"
+  },
+  {
+    category: "서울캠 장터게시판",
+    title: "원룸 300/46 회기역 30초 거리 비싸네요 이런 ㅠㅠ"
+  }
+]
 class HomeTabContentThree extends React.Component {
   
-  SampleCardItem = data => data.map(({category, title})=>(
-    <View style={{ flex:1, overflow:"hidden", paddingVertical:5, flexDirection:"row", alignItems:"center"} }>
+  SampleCardItem = data => data.map(({category, title}, index)=>(
+    <View key={`sampleOne_${index}`} 
+          style={{ 
+              flex:1,
+              overflow:"hidden",
+              paddingVertical:5,
+              flexDirection:"row",
+              alignItems:"center"
+            }}>
+
       <Text style={{fontWeight:"500", marginRight:15}}>{category}</Text>
       <Text numberOfLines={1} style={{flex:1, color:"grey", fontSize:13}}>{title}</Text>
       <Icon name="ios-home" style={{marginLeft:"auto", fontSize:10, color:"red"}}></Icon>
@@ -135,24 +159,6 @@ class HomeTabContentThree extends React.Component {
   ))
 
   render(){
-    const sempleOne=[
-      {
-        category: "서울캠 자유게시판",
-        title: "오랜만에 실로암 영상 보니까 웃기네 웃기네"
-      },
-      {
-        category: "비말게시판",
-        title: "인증 후 이용가능"
-      },
-      {
-        category: "졸업생게시판",
-        title: "이번주 업적"
-      },
-      {
-        category: "서울캠 장터게시판",
-        title: "원룸 300/46 회기역 30초 거리 비싸네요 이런 ㅠㅠ"
-      }
-    ]
     return(
       <SafeAreaView style={{paddingHorizontal:10}}>
 
@@ -168,15 +174,33 @@ class HomeTabContentThree extends React.Component {
   }
 }
 
+const anonyImage = "https://cdn2.iconfinder.com/data/icons/scenarium-vol-4/128/011_avatar_anonymous_profile_privacy_hacker_mask_hoodie-512.png"
+const ice = "https://www.creativefabrica.com/wp-content/uploads/2020/01/05/Ice-Cream-Kawaii-Cute-Illustration-Graphics-1-580x386.jpg"
+const items = [
+  {
+    image:"",id:"",date:"01/11 16:08",
+    category: "서울캠 자유게시판",
+    title: "이건 진짜 심각한 거 아님?",
+    content: "걍 대놓고 생까겠다는 거네 ㅋㅋㅋㅋㅋㅋ",
+    likes: "32",
+    comments: "27",
+  },
+  {
+    image: ice, id: "아이스크림", date: "01/11 16:08",
+    category:"서울캠 자유게시판",
+    title:"싱글벙글 추장관",
+    content:"아 ㅋㅋㅋ",
+    likes:"29",
+    comments:"27",
+  }
+]
 class HomeTabContentFour extends React.Component {
-  SampleCardItems=data=>{
-    const anonyImage = "https://cdn2.iconfinder.com/data/icons/scenarium-vol-4/128/011_avatar_anonymous_profile_privacy_hacker_mask_hoodie-512.png"
+  SampleCardItems=data=>{    
     return(
-      data.map( (item) => {
-        console.log(`item.map = ${item}`)
+      data.map( (item, index) => {
         return(
-          <>
-          <View style={{flexDirection:"row", alignItems:"center", marginVertical:10}}>
+          <View key={`sampleTwo_${index}`}>
+          <View  style={{flexDirection:"row", alignItems:"center", marginVertical:10}}>
             <Image source={item.image === "" ? { url: anonyImage } : {url: item.image}}
               style={{width:35, height:35, borderRadius:37.5}}/>          
             <Text style={{marginLeft:10, fontWeight:"800"}}>{item.id === "" ? "익명" : item.id}</Text>
@@ -197,32 +221,12 @@ class HomeTabContentFour extends React.Component {
               <Text style={{paddingHorizontal:3, color:"#79b"}}>{item.comments}</Text>
             </View>
           </View>
-          </>
+          </View>
         )}
       )
     )
   }
   render(){
-    const anonyImage = "https://cdn2.iconfinder.com/data/icons/scenarium-vol-4/128/011_avatar_anonymous_profile_privacy_hacker_mask_hoodie-512.png"
-    const ice = "https://www.creativefabrica.com/wp-content/uploads/2020/01/05/Ice-Cream-Kawaii-Cute-Illustration-Graphics-1-580x386.jpg"
-    const items = [
-      {
-        image:"",id:"",date:"01/11 16:08",
-        category: "서울캠 자유게시판",
-        title: "이건 진짜 심각한 거 아님?",
-        content: "걍 대놓고 생까겠다는 거네 ㅋㅋㅋㅋㅋㅋ",
-        likes: "32",
-        comments: "27",
-      },
-      {
-        image: ice, id: "아이스크림", date: "01/11 16:08",
-        category:"서울캠 자유게시판",
-        title:"싱글벙글 추장관",
-        content:"아 ㅋㅋㅋ",
-        likes:"29",
-        comments:"27",
-      }
-    ]
     return (
       <SafeAreaView style={{paddingHorizontal:10}}>
         <View style={{flexDirection:"row", marginBottom:15, alignItems:"center"}}>
