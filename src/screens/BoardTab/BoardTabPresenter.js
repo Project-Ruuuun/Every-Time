51988557797744f60d21a3a7f92f8d9d799e6ebb
yjw5344 <yjw5344@hanmail.net>
2020-01-15 19:09:09 +0900

    Date : 2020/01/15 -  BoardPush

--- src/screens/BoardTab/BoardTabPresenter.js
+++ src/screens/BoardTab/BoardTabPresenter.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, StyleSheet } from 'react-native';
-import { Container } from 'native-base';
 import BoardTabNavigator from './BoardNavigation/BoardTabRouter';  
-
-// const BoardTopTap = createMaterialTopTabNavigator(BoardTabNavigator);
 
 function BoardTabPresenter(props) {
 
   const {} = props;
 
-  return (
-    // <Container style={styles.container}>
-    //   <View>
-    //     <BoardTabNavigator/>
-    //     <Text>Hello</Text>
-    //   </View>
-    // </Container>
+  return (    
     <BoardTabNavigator/>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent:'center',
-    backgroundColor: 'white'
-  }
-});
 
 export default BoardTabPresenter;
